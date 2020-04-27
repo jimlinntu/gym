c4a6f62f7c6c003cf6f660b174bbcfccda71c607
jimlinntu <b04705003@ntu.edu.tw>
2020-04-27 21:32:41 +0800

    add 141 solution

---
+++ leetcode/141/main.cc
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *tortoise = head, *hare = head;
+        while(tortoise != NULL && hare != NULL && hare->next != NULL){
+            tortoise = tortoise->next;
+            hare = hare->next->next;
+            if(tortoise == hare){
+                return true;
+            }
+        }
+        return false;
+    }
+};
