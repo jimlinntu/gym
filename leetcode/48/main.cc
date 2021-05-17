55e77277bd41e2afafccfd4ca143f8bc9312af2b
Jim Lin <b04705003@ntu.edu.tw>
2021-05-17 17:23:49 +0800

    add 48 sol and fix wrong 48 to 76

--- leetcode/48/main.cc
+++ leetcode/48/main.cc
@@ -1,55 +1,26 @@
 class Solution {
 public:
-    bool inT(char c, unordered_map<char, int> &map_t){
-        return map_t.find(c) != map_t.end();
+    pair<int, int> next(int i, int j, int n){
+        return {j, n-1-i};
     }
-    string minWindow(string s, string t) {
-        int n = s.length();
-        int m = t.length();
-        
-        if(n == 0 or m == 0) return "";
-        
-        unordered_map<char, int> map_t;
-        unordered_map<char, int> window;
-        int min_window;
-        int x = 0, y = 0;
-        int best_x, best_y;
 
-        // Compute string t statistics
-        for(int i = 0; i < m; i++){
-            map_t[t[i]]++;
-        }
-        // Move y until we satisfy all t's unique character counts
-        for(auto it = map_t.begin(); it != map_t.end(); it++){
-            char c = it->first;
-            int count = it->second;
-            // check if window[c] >= count, if not, keep move forward
-            while(y < n and window[c] < count){
-                if(inT(s[y], map_t)) window[s[y]]++;
-                y++;
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int mid = n/2;
+        // row
+        for(int i = 0; i < mid; i++){
+            // col
+            for(int j = i; j < n-i-1; j++){
+                // Swap three times
+                // Ex. 4 8 -> 8 4 -> 6 4 -> 3 4
+                //     3 6    3 6    3 8    6 8
+                // 
+                int ii = i, jj = j;
+                for(int k = 0; k < 3; k++){
+                    tie(ii, jj) = next(ii, jj, n);
+                    swap(matrix[i][j], matrix[ii][jj]);
+                }
             }
-            // if window[c] still < count, we cannot find a string covering all t chars
-            if(window[c] < count) return "";
         }
-        min_window = y-x+1;
-        // Move x forward and then move y forward
-        while(x < y){
-            if(y-x < min_window){
-                min_window = y-x;
-                best_x = x, best_y = y;
-            }
-            char c = s[x++];
-            if(!inT(c, map_t)) continue; // if it is not a char in t, ignore it
-            int remain = (--window[c]); // move x forward, decrement window[c]
-            int count = map_t[c];
-            if(remain >= count) continue; // if char c still has enough char, keep going
-            // if not, we need to move y forward to compensate our character loss
-            while(y < n and window[c] < count){
-                if(inT(s[y], map_t)) window[s[y]]++;
-                y++;
-            }
-            if(window[c] < count) break;
-        }
-        return string(s.begin()+best_x, s.begin()+best_y);
     }
 };
