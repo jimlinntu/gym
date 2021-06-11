c421e69256d73416d5d68d9bac41da713e6da02c
Jim Lin <b04705003@ntu.edu.tw>
2021-06-11 10:57:50 +0800

    add 371 bit adding by XOR and AND

---
+++ leetcode/371/main-carry.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        // If they will still generate carry bits
+        while(((unsigned)(a & b) << 1) != 0){
+            int carry = (unsigned)(a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        // if they do not generate carry bits anymore, we will just add them together
+        return a ^ b;
+    }
+};
