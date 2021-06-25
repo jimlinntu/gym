60b182244b714ae0b49ac461d7a59f474f99cba8
Jim Lin <b04705003@ntu.edu.tw>
2021-06-25 22:00:01 +0800

    add 263 sol

---
+++ leetcode/263/main.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n <= 0) return false;
+        while(n % 2 == 0 or n % 3 == 0 or n % 5 == 0){
+            if(n % 2 == 0) n /= 2;
+            if(n % 3 == 0) n /= 3;
+            if(n % 5 == 0) n /= 5;
+            
+        }
+        return n == 1;
+    }
+};
