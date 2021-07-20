bbc802e5263e9d49288a2cfbebe75165a675fa69
Jim Lin <b04705003@ntu.edu.tw>
2021-07-20 23:04:36 +0800

    refine amazon fresh promotion's code

--- leetcode/amazon/fresh_promotion.cc
+++ leetcode/amazon/fresh_promotion.cc
@@ -35,13 +35,43 @@ bool is_winner(vector<vector<string>> &codeList,
     return true;
 }
 
+bool match(vector<string> &shoppingCart, int i, vector<string> &l){
+    int n = shoppingCart.size();
+    if(n - i < l.size()) return false; // no enough items to compare
+
+    for(int j = 0; j < l.size(); j++){
+        if(l[j] != "any" and shoppingCart[i+j] != l[j]) return false;
+    }
+    return true;
+}
+
+bool is_winner2(vector<vector<string>> &codeList,
+               vector<string> &shoppingCart){
+
+    int n = shoppingCart.size();
+    int p = 0; // shoppingCart[p] is the next one we want to compute
+
+    for(int i = 0; i < codeList.size(); i++){
+        auto &l = codeList[i];
+        while(p < n){
+            if(match(shoppingCart, p, l)) break;
+            else p++; // go to the next one to compare
+        }
+
+        if(p == n) return false;
+
+        p = p + l.size(); // go to the next one
+    }
+    return true;
+}
+
 int main(){
     vector<pair<vector<vector<string>>, vector<string>>>\
             testcases = {{{{"a", "a"}, {"b", "any", "b"}}, {"o", "a", "a", "b", "o", "b"}},
                          {{{"a", "a"}, {"b", "any", "b"}}, {"a", "a", "o", "o", "b", "b", "a", "b", "b"}},
                          {{{"a", "a"}, {"b", "any", "b"}}, {"a", "b", "a", "b", "o", "b"}}};
     for(auto &t: testcases){
-        cout << is_winner(t.first, t.second) << endl;
+        cout << is_winner2(t.first, t.second) << endl;
     }
     return 0;
 }
