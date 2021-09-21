b37c7c63ef158ceb337a1330d8a2f1c37df0d8d7
Jim Lin <b04705003@ntu.edu.tw>
2021-09-22 06:11:06 +0800

    add 1396

---
+++ leetcode/1396/main.cc
@@ -0,0 +1,45 @@
+truct Avg{
+    int acc;
+    int n;
+};
+
+struct CheckIn{
+    string name;
+    int t;
+};
+
+class UndergroundSystem {
+public:
+    map<pair<string, string>, Avg> map;
+    unordered_map<int, CheckIn> track_enter;
+    UndergroundSystem() {
+        
+    }
+    
+    void checkIn(int id, string stationName, int t) {
+        track_enter[id] = {stationName, t};
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        // update
+        CheckIn &c = track_enter[id];
+        Avg &e = map[{c.name, stationName}];
+        e.acc += t-c.t;
+        e.n++;
+        // destroy the checkin (or just leave it there)
+        // track_enter.erase(id);
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        Avg &e = map[{startStation, endStation}];
+        return (double)e.acc / e.n;
+    }
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
